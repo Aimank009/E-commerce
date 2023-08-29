f3af08c6cbf2852e8cf35b322c43a1b25fbb1341
Aiman <aiman@Aimans-MacBook-Air.local>
2023-08-29 23:23:25 +0530

    Create Components and Navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import Navigation from "./customer/components/Navigation/Navigation"
+import HomePage from './customer/pages/HomaPage/HomePage';
 
 function App() {
   return (
-    <div className="App">
-     hello ecommerce
+    <div className="">
+      <Navigation />
+      <div>
+        <HomePage/>
+      </div>
     </div>
   );
 }

293795058f84fb8b477b8ccf1e00fc531c8d3b15
Aiman <aiman@Aimans-MacBook-Air.local>
2023-09-01 23:32:18 +0530

    Create Order Card

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Cart from './customer/components/Cart/Cart';
 import Checkout from './customer/components/Checkout/Checkout';
 import Footer from './customer/components/Footer/Footer';
 import Navigation from "./customer/components/Navigation/Navigation"
+import Order from './customer/components/Order/Order';
 import ProductDetails from './customer/components/ProductDetails/ProductDetails';
 import Product from './customer/components/Products/Product';
 import HomePage from './customer/pages/HomePage/HomePage.jsx';
@@ -17,7 +18,8 @@ function App() {
         {/* <Product/> */}
         {/* <ProductDetails/> */}
         {/* <Cart/> */}
-        <Checkout/>
+        {/* <Checkout/> */}
+        <Order/>
       </div>
       <Footer/>
     </div>

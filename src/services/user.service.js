f4f620e12a2787042fe4daa4cbf460fbd8d275ad
Aiman <aiman@Aimans-MacBook-Air.local>
2023-09-03 01:05:21 +0530

    Create authcontroller for login and register

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -34,7 +34,7 @@ const findUserById = async (userId) => {
     throw new Error(error.message);
   }
 };
-const findUserByEmail = async (email) => {
+const getUserByEmail = async (email) => {
   try {
     const user = await User.findOne({ email });
     if (!user) {
@@ -70,7 +70,7 @@ const getAllUsers = async () => {
 module.exports = {
   createUser,
   findUserById,
-  findUserByEmail,
+  getUserByEmail,
   getuserProfileByToken,
   getAllUsers,
 };
